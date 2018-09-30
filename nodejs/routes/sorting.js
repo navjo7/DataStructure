0105fdf4053d1ea7d2ce88836bec59e00bd87baf
navjo7 <navjotsinghdata@gmail.com>
2018-09-30 21:34:21 +1000

    bubble and insertion  sort  nodejs

--- nodejs/routes/sorting.js
+++ nodejs/routes/sorting.js
@@ -24,11 +24,42 @@ router.get('/selection',(req,res,next)=>{
 })
 //bubble sort
 router.get('/bubble',(req,res,next)=>{
-
+    let unsortedArray = [5, 3, 6, 8, 2, 1, 4, 5, 6]
+    let testUnsortedArray = [...unsortedArray]
+    for (let i = 0; i < testUnsortedArray.length-1; i++) {
+        for(let j = 0; j<testUnsortedArray.length-i-1;j++){
+            if(testUnsortedArray[j]>testUnsortedArray[j+1]){
+                let temp = testUnsortedArray[j]
+                testUnsortedArray[j] = testUnsortedArray[j+1]
+                testUnsortedArray[j+1] = temp
+            }
+        }
+    }
+    res.status(200).json({
+        sorted: testUnsortedArray
+    })
+    console.log("sorted array: ", JSON.stringify(testUnsortedArray))
+    console.log("Unsorted array: ", JSON.stringify(unsortedArray))
 })
-//selection sort
+//insertion sort
 router.get('/insertion', (req, res, next) => {
-
+    let unsortedArray = [5, 3, 6, 8, 2, 1, 4, 5, 6]
+    let testUnsortedArray = [...unsortedArray]
+    for (let i = 0; i < testUnsortedArray.length-1; i++) {
+        for(let j = i+1; j>0;j--){
+            if(testUnsortedArray[j]<testUnsortedArray[j-1]){
+                let temp = testUnsortedArray[j]
+                testUnsortedArray[j] = testUnsortedArray[j-1]
+                testUnsortedArray[j-1] = temp
+            }
+        }
+        console.log("%%% array: ", JSON.stringify(testUnsortedArray))
+    }
+    res.status(200).json({
+        sorted: testUnsortedArray
+    })
+    console.log("sorted array: ", JSON.stringify(testUnsortedArray))
+    console.log("Unsorted array: ", JSON.stringify(unsortedArray))
 })
 
 module.exports = router
